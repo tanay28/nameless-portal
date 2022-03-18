5847740fcded821dec8178101c6191f2f55b1254
Tanay <tmtanay56@gmail.com>
2022-03-19 02:04:18 +0530

    fbconfig added in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAQcYC96wE_UuO3KzbWf_4HRwOLDAZkOyY",
+    authDomain: "namelessmanage.firebaseapp.com",
+    projectId: "namelessmanage",
+    storageBucket: "namelessmanage.appspot.com",
+    messagingSenderId: "892425044390",
+    appId: "1:892425044390:web:eedae4199f5ef342ab4f67",
+    measurementId: "G-2JM0BERMXT"
+  }
 };
